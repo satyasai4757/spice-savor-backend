7a1fe6ae6ad9f9ca8082e83e4b28b49ea3445cf0
Satya-diverse <satya.p@diverseinfotech.com>
2026-01-05 15:08:20 +0530

    new recipe.model changes

--- src/models/recipe.model.js
+++ src/models/recipe.model.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
-const recipeSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    cookingTime: String,
-    summary: String,
-    level: String,
-    category: String,
-    image: String,
-    author: String,
-    ingredients: [String],
-    steps: [String]
-  },
-  { timestamps: true }
-);
+const recipeSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  cookingTime: { type: String, required: true },
+  summary: { type: String, required: true },
+  level: { type: String, enum: ['Easy', 'Medium', 'Hard'], required: true },
+  category: { type: String, enum: ['Veg', 'Non-Veg'], required: true },
+  image: { type: [String], required: true },
+  author: { type: String, required: true },
+  ingredients: { type: String, required: true },
+  steps: { type: [String], required: true }
+}, { timestamps: true });
+
 
 module.exports = mongoose.model('Recipe', recipeSchema);
